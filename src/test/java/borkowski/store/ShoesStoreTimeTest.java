a4da2247f12d34dddd412c3b06a70966bfdcdb22
Michal Borkowski <borkov@op.pl>
2019-11-01 13:36:41 +0100

    add test for Add

--- src/test/java/borkowski/store/ShoesStoreTimeTest.java
+++ src/test/java/borkowski/store/ShoesStoreTimeTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.when;
@@ -18,7 +20,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class ShoesStoreTimeTest {
 
     private ShoesStoreImpl service = new ShoesStoreImpl();
-
+   // private ArrayList<Shoes> expectedShoes = new ArrayList<>();
     @Rule
     public final ExpectedException exception = ExpectedException.none();
 
@@ -33,36 +35,43 @@ public class ShoesStoreTimeTest {
     private void assertNotNull(long readTime) {
     }
 
-
     @Test   //test dla metody read
     public void testOfReadTimeOnReadMethod(){
         long time = 222222;
         when(timeStamp.getTimeNow()).thenReturn((long)222222);
-        Shoes shoes = new Shoes(103L,35,"Puma","grey");
+        Shoes shoes = new Shoes(100L,35,"Puma","grey");
         service.create(shoes);
         service.setTime(timeStamp.getTimeNow());
-        service.read(103L);
-        assertEquals(time, service.read(103L).getReadTime());
+        service.read(100L);
+        assertEquals(time, service.read(100L).getReadTime());
     }
     @Test
     public void testOfReadTimeOnReadAllMethod(){
         long time = 222222;
         when(timeStamp.getTimeNow()).thenReturn(time);
-        Shoes shoes1 = new Shoes(100L,39,"Puma","grey");
-        Shoes shoes2 = new Shoes(101L,36,"Nike","red");
-        Shoes shoes3 = new Shoes(102L,42,"Adidas","white");
+        Shoes shoes1 = new Shoes(101L,39,"Puma","grey");
+        Shoes shoes2 = new Shoes(102L,36,"Nike","red");
+        Shoes shoes3 = new Shoes(103L,42,"Adidas","white");
 
         service.create(shoes1);
         service.create(shoes2);
         service.create(shoes3);
         service.setTime(timeStamp.getTimeNow());
 
-
-
-
         assertEquals(time, service.readAll().get(0).getReadTime());
         assertEquals(time, service.readAll().get(1).getReadTime());
         assertEquals(time, service.readAll().get(2).getReadTime());
     }
+
+    @Test
+    public void testOfAddTimeOnCreateMethod() {
+        long time = 1234567891;
+        when(timeStamp.getTimeNow()).thenReturn(time);
+        Shoes shoes = new Shoes(104L,42,"Adidas","white");
+        service.setTime(timeStamp.getTimeNow());
+        service.create(shoes);
+        assertEquals(time, service.read(104L).getAddTime());
+    }
+
 }
 
